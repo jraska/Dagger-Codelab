7b90a954b1822ad0f840378ed8e614753752896f
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-03-27 00:23:29 +0100

    Added multibinding example

--- app/src/androidTest/java/com/jraska/dagger/codelab/app/AppTest.kt
+++ app/src/androidTest/java/com/jraska/dagger/codelab/app/AppTest.kt
@@ -1,6 +1,5 @@
 package com.jraska.dagger.codelab.app
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
@@ -12,10 +11,7 @@ import androidx.test.rule.ActivityTestRule
 import org.hamcrest.CoreMatchers.not
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 
-@RunWith(JUnit4::class)
 class AppTest {
   @get:Rule
   val activityRule = ActivityTestRule(MainActivity::class.java)
---
+++ core/src/main/java/com/jraska/dagger/codelab/core/config/RemoteConfigEntry.kt
@@ -0,0 +1,6 @@
+package com.jraska.dagger.codelab.core.config
+
+class RemoteConfigEntry(
+  val key: String,
+  val default: Boolean
+)
