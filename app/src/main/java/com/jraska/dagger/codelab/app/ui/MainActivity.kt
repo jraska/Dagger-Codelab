3f284791c3518c28eb79b9324c3ddcef17a770c2
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-03-30 22:57:40 +0200

    Remove unnecessary complexity

--- app/src/main/java/com/jraska/dagger/codelab/app/ui/MainActivity.kt
+++ app/src/main/java/com/jraska/dagger/codelab/app/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jraska.dagger.codelab.app.R
-import com.jraska.dagger.codelab.config.ui.ConfigActivity
 import com.jraska.dagger.codelab.core.analytics.AnalyticsEvent
 import com.jraska.dagger.codelab.core.analytics.EventAnalytics
 
@@ -19,7 +18,6 @@ class MainActivity : AppCompatActivity() {
 
     findViewById<View>(R.id.fab).setOnClickListener {
       eventAnalytics.reportEvent(AnalyticsEvent.create("main_onFabClick"))
-      ConfigActivity.start(this)
     }
   }
 }
