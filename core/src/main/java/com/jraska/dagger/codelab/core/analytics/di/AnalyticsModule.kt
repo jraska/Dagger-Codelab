0bffea80253823c98cf951c7ddee7745f44ed6a2
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-03-30 20:23:40 +0200

    Complete Section 1

--- core/src/main/java/com/jraska/dagger/codelab/core/analytics/di/AnalyticsModule.kt
+++ core/src/main/java/com/jraska/dagger/codelab/core/analytics/di/AnalyticsModule.kt
@@ -1,4 +1,23 @@
 package com.jraska.dagger.codelab.core.analytics.di
 
+import com.jraska.dagger.codelab.core.analytics.EventAnalytics
+import com.jraska.dagger.codelab.core.analytics.LibraryEventAnalytics
+import com.jraska.dagger.codelab.core.analytics.lib.AnalyticsLibrary
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
 object AnalyticsModule {
+
+  @Provides
+  @Singleton
+  fun provideAnalyticsLibrary(): AnalyticsLibrary {
+    return AnalyticsLibrary()
+  }
+
+  @Provides
+  fun provideEventAnalytics(implementation: LibraryEventAnalytics): EventAnalytics {
+    return implementation
+  }
 }
