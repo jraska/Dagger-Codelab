9b0921e0dba0966d71d1c37d800a16ac410a317c
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-03-30 23:39:18 +0200

    Implement section 2

--- app/src/main/java/com/jraska/dagger/codelab/app/DaggerApp.kt
+++ app/src/main/java/com/jraska/dagger/codelab/app/DaggerApp.kt
@@ -5,14 +5,18 @@ import android.app.Application
 import androidx.fragment.app.Fragment
 import com.jraska.dagger.codelab.app.di.AppComponent
 import com.jraska.dagger.codelab.app.di.DaggerAppComponent
+import com.jraska.dagger.codelab.core.analytics.di.AnalyticsComponent
+import com.jraska.dagger.codelab.core.analytics.di.DaggerAnalyticsComponent
 import com.jraska.dagger.codelab.core.di.HasAppComponent
 
 open class DaggerApp : Application(), HasAppComponent {
-  val appComponent: AppComponent by lazy {
-    DaggerAppComponent.builder()
+  val appComponent: AppComponent get() {
+    return DaggerAppComponent.builder()
       .setContext(this)
       .build()
   }
+
+  val analyticsComponent: AnalyticsComponent by lazy { createAnalyticsComponent() }
 
   override fun appComponent(): Any {
     return appComponent
@@ -20,8 +24,9 @@ open class DaggerApp : Application(), HasAppComponent {
 
   override fun onCreate() {
     super.onCreate()
-    appComponent.onAppCreateActions().forEach { it.onCreate() }
   }
+
+  private fun createAnalyticsComponent(): AnalyticsComponent = DaggerAnalyticsComponent.create()
 
   companion object {
     fun of(activity: Activity): DaggerApp {
