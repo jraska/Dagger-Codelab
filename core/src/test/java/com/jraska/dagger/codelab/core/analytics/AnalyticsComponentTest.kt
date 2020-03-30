a5490ce85426908652b69aec0cb86e20ac94b2c0
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-03-30 20:23:40 +0200

    Complete Section 1

--- core/src/test/java/com/jraska/dagger/codelab/core/analytics/AnalyticsComponentTest.kt
+++ core/src/test/java/com/jraska/dagger/codelab/core/analytics/AnalyticsComponentTest.kt
@@ -1,24 +1,17 @@
 package com.jraska.dagger.codelab.core.analytics
 
-import com.jraska.dagger.codelab.core.analytics.di.AnalyticsComponent
-import com.jraska.dagger.codelab.core.analytics.lib.AnalyticsLibrary
+import com.jraska.dagger.codelab.core.analytics.di.DaggerAnalyticsComponent
 import org.junit.Test
 
 class AnalyticsComponentTest {
   @Test
   fun componentDemo() {
-    val analyticsComponent = object : AnalyticsComponent {
-      val eventAnalytics = LibraryEventAnalytics(AnalyticsLibrary(), AnalyticsFilter())
-
-      override fun eventAnalytics(): LibraryEventAnalytics {
-        return eventAnalytics
-      }
-    }
+    val analyticsComponent = DaggerAnalyticsComponent.create()
 
     analyticsComponent.eventAnalytics().reportEvent(AnalyticsEvent.create("One"))
     analyticsComponent.eventAnalytics().reportEvent(AnalyticsEvent.create("Two"))
 
-    val analyticsReported = analyticsComponent.eventAnalytics().library.inMemoryData
+    val analyticsReported = (analyticsComponent.eventAnalytics() as LibraryEventAnalytics).library.inMemoryData
     println(analyticsReported)
     assert(analyticsReported.size == 2)
   }
