1e16dc2088ba2be7709d14143af98b051672bd7c
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-04-01 00:45:41 +0200

    Implement Section 3

--- feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigComponent.kt
+++ feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigComponent.kt
@@ -1,6 +1,9 @@
 package com.jraska.dagger.codelab.config.di
 
+import com.jraska.dagger.codelab.config.ui.ConfigActivity
+
 const val CONFIG_BYE_BUTTON = "bye_button"
 
 interface ConfigComponent {
+  fun inject(configActivity: ConfigActivity)
 }
