005b9e80fae7e6618d55e8e010f4d3d7fb906372
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-04-05 21:53:30 +0200

    Solution of section 4

--- feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigModule.kt
+++ feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigModule.kt
@@ -1,11 +1,14 @@
 package com.jraska.dagger.codelab.config.di
 
+import com.jraska.dagger.codelab.config.ExpensiveConfig
 import com.jraska.dagger.codelab.config.MultibindInMemoryConfig
 import com.jraska.dagger.codelab.config.MutableConfig
+import com.jraska.dagger.codelab.core.app.OnAppCreate
 import com.jraska.dagger.codelab.core.config.RemoteConfig
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
+import dagger.multibindings.IntoSet
 import dagger.multibindings.StringKey
 import javax.inject.Singleton
 
@@ -23,4 +26,13 @@ object ConfigModule {
   @IntoMap
   @StringKey(CONFIG_BYE_BUTTON)
   fun byeButtonConfig() = true
+
+  @Provides
+  @IntoMap
+  @StringKey(ExpensiveConfig.KEY)
+  fun expensiveConfig() = ExpensiveConfig.value
+
+  @Provides
+  @IntoSet
+  fun expensiveConfigInitializer(): OnAppCreate = ExpensiveConfig.Initializer
 }
