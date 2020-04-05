74fda5633f301544dc35519d21815b8653bcb46a
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-04-05 17:54:36 +0200

    Implement section 4

--- feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigModule.kt
+++ feature/config/src/main/java/com/jraska/dagger/codelab/config/di/ConfigModule.kt
@@ -1,10 +1,12 @@
 package com.jraska.dagger.codelab.config.di
 
-import com.jraska.dagger.codelab.config.InMemoryConfig
+import com.jraska.dagger.codelab.config.MultibindInMemoryConfig
 import com.jraska.dagger.codelab.config.MutableConfig
 import com.jraska.dagger.codelab.core.config.RemoteConfig
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.IntoMap
+import dagger.multibindings.StringKey
 import javax.inject.Singleton
 
 @Module
@@ -12,8 +14,13 @@ object ConfigModule {
 
   @Singleton
   @Provides
-  fun remoteConfig(config: InMemoryConfig): MutableConfig = config
+  fun remoteConfig(config: MultibindInMemoryConfig): MutableConfig = config
 
   @Provides
   fun inMemoryConfig(config: MutableConfig): RemoteConfig = config
+
+  @Provides
+  @IntoMap
+  @StringKey(CONFIG_BYE_BUTTON)
+  fun byeButtonConfig() = true
 }
