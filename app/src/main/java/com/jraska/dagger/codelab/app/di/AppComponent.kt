b0e46f953f1785f4e1191f5e4e9da2c7f129fc06
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-04-12 22:50:00 +0200

    Update AppComponent

--- app/src/main/java/com/jraska/dagger/codelab/app/di/AppComponent.kt
+++ app/src/main/java/com/jraska/dagger/codelab/app/di/AppComponent.kt
@@ -2,14 +2,23 @@ package com.jraska.dagger.codelab.app.di
 
 import android.content.Context
 import com.jraska.dagger.codelab.app.ui.MainActivity
+import com.jraska.dagger.codelab.app.ui.MainFragment
+import com.jraska.dagger.codelab.config.di.ConfigComponent
+import com.jraska.dagger.codelab.config.di.ConfigModule
 import com.jraska.dagger.codelab.core.analytics.di.AnalyticsModule
+import com.jraska.dagger.codelab.core.app.OnAppCreate
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AnalyticsModule::class])
-interface AppComponent {
+@Component(modules = [AnalyticsModule::class, ConfigModule::class])
+interface AppComponent : ConfigComponent {
+
+  fun onAppCreateActions(): Set<OnAppCreate>
+
+  fun inject(mainFragment: MainFragment)
+
   fun inject(mainActivity: MainActivity)
 
   @Component.Builder
