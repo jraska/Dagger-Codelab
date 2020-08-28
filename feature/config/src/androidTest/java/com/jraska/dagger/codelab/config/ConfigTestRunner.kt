c46c1d1ea9f9168993cda366cec6a09700969098
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-08-28 19:59:16 +0200

    Move the ConfigTest into Hilt

--- feature/config/src/androidTest/java/com/jraska/dagger/codelab/config/ConfigTestRunner.kt
+++ feature/config/src/androidTest/java/com/jraska/dagger/codelab/config/ConfigTestRunner.kt
@@ -3,10 +3,11 @@
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
 
-@Suppress("unused") // used in build.gradle
+ @Suppress("unused") // used in build.gradle
 class ConfigTestRunner : AndroidJUnitRunner() {
   override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
-    return super.newApplication(cl, ConfigTestApp::class.qualifiedName, context)
+    return super.newApplication(cl, HiltTestApplication::class.qualifiedName, context)
   }
 }
