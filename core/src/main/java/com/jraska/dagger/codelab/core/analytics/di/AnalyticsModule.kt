35e540b6266273bec080a19ec5ccffad13c27512
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-09-10 23:58:16 +0200

    Dagger Hilt Setup in the project

--- core/src/main/java/com/jraska/dagger/codelab/core/analytics/di/AnalyticsModule.kt
+++ core/src/main/java/com/jraska/dagger/codelab/core/analytics/di/AnalyticsModule.kt
@@ -5,6 +5,7 @@ import com.jraska.dagger.codelab.core.analytics.LibraryEventAnalytics
 import com.jraska.dagger.codelab.core.analytics.lib.AnalyticsLibrary
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.migration.DisableInstallInCheck
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
